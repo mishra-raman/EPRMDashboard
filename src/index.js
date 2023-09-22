c1103e2f283b20c1a508a9c2185900aa55e8dcf6
mishra-raman <mishraraman94us@gmail.com>
2023-09-22 13:36:00 -0400

    netlify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Chart } from 'chart.js';
+// import { Chart } from 'chart.js';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
